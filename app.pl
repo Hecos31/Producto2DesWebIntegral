0d0c7bdecaed9ebfd567b03f89f4a969f5de758e
Hector <hectordz515@gmail.com>
2026-06-27 13:52:40 -0600

    Paso 3 con Perl

--- app.pl
+++ app.pl
@@ -1,55 +1,25 @@
 use Mojolicious::Lite -signatures;
-use SOAP::Lite;
-use Mojo::URL;
+use Lingua::Num2Word;
+use utf8;
 
-sub consumir_soap ($numero) {
-    my $client = SOAP::Lite
-        ->proxy('https://www.dataaccess.com/webservicesserver/NumberConversion.wso')
-        ->uri('http://www.dataaccess.com/webservicesserver/');
-
-    my $method = SOAP::Data->name('NumberToWords')
-        ->attr({xmlns => 'http://www.dataaccess.com/webservicesserver/'});
-    my $param = SOAP::Data->name('ubiNum')->value($numero);
-
-    my $response = $client->call($method => $param);
-
-    return "Error SOAP" if $response->fault;
-
-    my $resultado = $response->result;
-    $resultado =~ s/^\s+|\s+$//g if defined $resultado;
-    
-    return $resultado;
-}
-
-# Ruta exclusiva para el Paso 2 (http://localhost:3000/paso2/25)
-get '/paso2/:numero' => sub ($c) {
+# Ruta exclusiva para el Paso 3 (ej. http://localhost:3000/paso3/25)
+get '/paso3/:numero' => sub ($c) {
     my $numero = $c->param('numero');
     
-    my $resultado_ingles = consumir_soap($numero);
+    my $conversor = Lingua::Num2Word->new();
     
-    my $url = Mojo::URL->new('https://api.mymemory.translated.net/get');
-    $url->query(q => $resultado_ingles, langpair => 'en|es');
+    my $resultado_letras = $conversor->cardinal('spa', $numero);
     
-    my $tx = $c->ua->get($url);
-    my $resultado_espanol = "Error en la traducción";
-    
-    if (my $res = $tx->result) {
-        if ($res->is_success) {
-            $resultado_espanol = $res->json('/responseData/translatedText');
-        }
-    }
-    
-    $c->render(inline => <<~'HTML', numero => $numero, ingles => $resultado_ingles, espanol => $resultado_espanol);
+    $c->render(inline => <<~'HTML', numero => $numero, letras => ucfirst($resultado_letras));
       <html>
         <head>
           <meta charset="utf-8">
-          <title>Paso 2 - Traducción Perl</title>
+          <title>Paso 3 - Librería Perl</title>
         </head>
         <body>
-          <h2>Paso 2: Consumo SOAP y Traducción (Perl)</h2>
-          <p><strong>Número enviado:</strong> <%= $numero %></p>
-          <p><strong>Resultado de la función:</strong> <%= $ingles %></p>
-          <p><strong>Resultado final traducido a español:</strong> <%= $espanol %></p>
+          <h2>Paso 3: Conversión de Número a Letras (Librería CPAN)</h2>
+          <p><strong>Número ingresado:</strong> <%= $numero %></p>
+          <p><strong>Resultado con 'Lingua::Num2Word':</strong> <%= $letras %></p>
         </body>
       </html>
     HTML
