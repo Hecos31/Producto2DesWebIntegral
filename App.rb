24a2203f5e5af609ae5efc26de0bd40876ff23f3
Hector <hectordz515@gmail.com>
2026-06-27 13:07:50 -0600

    Paso 3 con ruby

--- App.rb
+++ App.rb
@@ -3,39 +3,23 @@ require 'savon'
 require 'net/http'
 require 'uri'
 require 'json'
-
-# Esta función encapsula el consumo del servicio SOAP.
-def consumir_soap(numero)
-  client = Savon.client(wsdl: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL')
-  response = client.call(:number_to_words, message: { "ubiNum" => numero })
-  
-  # Retornamos el resultado limpio
-  response.body[:number_to_words_response][:number_to_words_result].strip
-end
+require 'humanize'
 
 # Ruta que recibe el número en la URL (ej. http://localhost:4567/paso2/25)
-get '/paso2/:numero' do
+get '/paso3/:numero' do
   numero = params[:numero].to_i
-  
-  resultado_ingles = consumir_soap(numero)
-  
-  url_traduccion = URI("https://api.mymemory.translated.net/get?q=#{URI.encode_www_form_component(resultado_ingles)}&langpair=en|es")
-  respuesta_http = Net::HTTP.get(url_traduccion)
-  datos_traduccion = JSON.parse(respuesta_http)
-  
-  resultado_espanol = datos_traduccion['responseData']['translatedText']
+  resultado_letras = numero.humanize(locale: :es)
   
   <<~HTML
     <html>
       <head>
         <meta charset="utf-8">
-        <title>Paso 2 - Traducción</title>
+        <title>Paso 3 - Usando Librería</title>
       </head>
       <body>
-        <h2>Paso 2: Consumo SOAP y Traducción</h2>
-        <p><strong>Número enviado:</strong> #{numero}</p>
-        <p><strong>Resultado de la función (Paso 1):</strong> #{resultado_ingles}</p>
-        <p><strong>Resultado final traducido a español:</strong>#{resultado_espanol}</p>
+        <h2>Paso 3: Conversión de Número a Letras</h2>
+        <p><strong>Número ingresado:</strong> #{numero}</p>
+        <p><strong>Resultado con 'humanize':</strong> #{resultado_letras.capitalize}</p>
       </body>
     </html>
   HTML
